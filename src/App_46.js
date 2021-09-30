1a603363b77427562b196834e09e92a557f9d35c
zero0914 <208417146@gms.tku.edu.tw>
2021-09-30 19:54:41 +0800

    first commit

---
+++ src/App_46.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Todo_46 from './components/Todo_46';
+
+function App_46() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo_46 text='Sunny' />
+      <Todo_46 text='Rainy' />
+      <Todo_46 text='Windy' />
+    </div>
+  );
+}
+
+export default App_46;
---
+++ src/components/Todo_46.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Todo_46(props) {
+    return (
+        <div>
+            <div className="card">
+                <h2>{props.text}</h2>
+                <div className="actions">
+                    <button className="btn">Delete</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Todo_46
